e495a8c31f2266953fe6bcddbc24301943a6f874
Anwar Ullah Khan <125487732+zaryabwazir@users.noreply.github.com>
2023-08-13 02:19:39 +0500

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(-!(svf$&d!z^vdqn86lxdr3n*%ud-qt=+ewv^3@4lplurdbdo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
